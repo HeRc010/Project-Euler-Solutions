e970101b52ebff54d603c69fc8481b1f946741ad
Andrew L <adlawren010@gmail.com>
2016-06-12 01:29:17 -0700

    Add solution for problem 12.

--- Problems10-19/Problem12/solution.cpp
+++ Problems10-19/Problem12/solution.cpp
@@ -1,135 +1,40 @@
 /*
-   This takes a while to run; the answer for N = 2000000 is: 142913828922
+   This takes a while to run; the answer for N = 500 is: 76576500, with 576 divisors
 */
 
 #include <iostream>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 // parameters
-unsigned N = 500;
-map<float, vector<float>> cache;
+const unsigned N = 500;
 
-bool contains(const vector<float> &vec, float val)
-{
-   for (unsigned i = 0; i < vec.size(); ++i)
-   if (vec[i] == val)
-   {
-      return true;
-   }
+vector<unsigned> get_divisors(unsigned value) {
+  vector<unsigned> factors;
+  for (unsigned i = 1; i <= value; ++i) {
+    if ( (value % i) == 0 ) {
+      factors.push_back(i);
+    }
+  }
 
-   return false;
+  return factors;
 }
 
-vector<float> get_factors(float number)
-{
-   vector<float> factors;
-   factors.push_back(number);
+int main() {
+  unsigned i = 1, next_value = 1;
+  while (true) {
+    vector<unsigned> divisors = get_divisors(next_value);
 
-   if (number == 1.0f)
-      return factors;
+    cout << "Next number: " << next_value << ", factor count: " << divisors.size() << endl;
 
-   factors.push_back(1.0f);
+    if (divisors.size() > N) break;
 
-   for (float i = 2.0f; i < number; ++i)
-   {
-      if ((unsigned(number) % unsigned(i)) == 0)
-         factors.push_back(i);
-   }
+    i += 1;
+    next_value += i;
+  }
 
-   return factors;
-}
+  cout << "First triangular number with more than " << N << " divisors is: " << next_value << endl;
 
-vector<float> get_factors_other(float number)
-{
-   vector<float> factors;
-   // factors.push_back(number);
-   //
-   // if (number == 1.0f)
-   //    return factors;
-   //
-   // factors.push_back(1.0f);
-
-   auto entry = cache.find(number);
-   if (entry != cache.end())
-   {
-      return entry->second;
-   }
-
-   for (float i = 2.0f; i < number; ++i)
-   {
-      if ((unsigned(number) % unsigned(i)) == 0)
-      {
-         factors.push_back(i);
-         vector<float> nested_factors = get_factors_other(number/i);
-      }
-   }
-
-   // add factors to cache
-   cache[number] = factors;
-
-   return factors;
-}
-
-unsigned get_n_triangle(unsigned n)
-{
-   unsigned val = 0;
-   for (unsigned i = 1; i <= n; ++i)
-   {
-      val += i;
-   }
-
-   return val;
-}
-
-int main()
-{
-   vector<float> factors; // = get_factors_other(28.0f); // test
-
-   // for (unsigned i = 0; i < factors.size(); ++i)
-   // {
-   //    cout << factors[i] << endl;
-   // }
-   //
-   // return 0;
-
-   // float i = 0.0f;
-   // //for (; factors.size() <= N; ++i)
-   // while (factors.size() <= N)
-   // {
-   //    ++i;
-   //    factors = get_factors(float(get_n_triangle(i)));
-   // }
-
-   // float j = 0.0f;
-   // //for (; factors.size() <= N;)
-   // while (factors.size() <= N)
-   // {
-   //    j = j + (j + 1);
-   //
-   //    factors = get_factors(float(j));
-   // }
-
-   float next_triangle = 0.0f;
-   for (unsigned k = 1; (factors.size() + 2) <= N; ++k)
-   {
-      next_triangle += k;
-
-      factors = get_factors_other(float(next_triangle));
-   }
-
-   cout << "number with over " << N << " factors is: " << next_triangle << endl;
-
-   // for (auto &p : cache)
-   // {
-   //    cout << p.first << " :" << endl;
-   //    for (auto &item : p.second)
-   //    {
-   //       cout << item << endl;
-   //    }
-   // }
-
-   return 0;
+  return 0;
 }
